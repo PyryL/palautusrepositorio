677b5d0e5822686a19cd39aa2c11804e24306ad6
PyryL <pyry@pyry.info>
2022-11-15 12:53:33 +0200

    v3t1

--- viikko3/nhl-reader/src/player.py
+++ viikko3/nhl-reader/src/player.py
@@ -1,6 +1,14 @@
 class Player:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, name, team, nationality, goals, assists):
+        self._name = name
+        self._team = team
+        self._nationality = nationality
+        self._goals = goals
+        self._assists = assists
+    
+    @property
+    def nationality(self):
+        return self._nationality
     
     def __str__(self):
-        return self.name
+        return f"{self._name} team {self._team} goals {self._goals} assists {self._assists}"
