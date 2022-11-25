feafb290fd6656281b8776e7989f2f088c30c0fd
PyryL <pyry@pyry.info>
2022-11-25 16:33:20 +0200

    v4t3

---
+++ viikko4/verkkokauppa/src/tests/kauppa_test.py
@@ -0,0 +1,62 @@
+import unittest
+from unittest.mock import Mock, ANY
+from kauppa import Kauppa
+from viitegeneraattori import Viitegeneraattori
+from varasto import Varasto
+from tuote import Tuote
+
+class TestKauppa(unittest.TestCase):
+    def setUp(self):
+        self.pankki_mock = Mock()
+        viitegeneraattori_mock = Mock()
+        viitegeneraattori_mock.uusi.return_value = 45
+        varasto_mock = Mock()
+        varasto_mock.saldo.side_effect = self.varasto_saldo
+        varasto_mock.hae_tuote.side_effect = self.varasto_hae_tuote
+        self.kauppa = Kauppa(varasto_mock, self.pankki_mock, viitegeneraattori_mock)
+        self.kauppa.aloita_asiointi()
+
+    def varasto_saldo(self, tuote_id):
+        if tuote_id == 1:
+            return 10
+        elif tuote_id == 2:
+            return 8
+        elif tuote_id == 3:
+            return 0
+
+    def varasto_hae_tuote(self, tuote_id):
+        if tuote_id == 1:
+            return Tuote(1, "maito", 5)
+        elif tuote_id == 2:
+            return Tuote(2, "juusto", 7)
+        elif tuote_id == 3:
+            return Tuote(3, "kahvi", 3)
+
+    def test_ostoksen_paaytyttya_pankin_metodia_tilisiirto_kutsutaan(self):
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.tilimaksu("pekka", "12345")
+        self.pankki_mock.tilisiirto.assert_called()
+
+    def test_ostoksen_paaytyttya_pankin_metodilla_tilisiirto_oikeat_parametrit(self):
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.tilimaksu("pekka", "12345")
+        self.pankki_mock.tilisiirto.assert_called_with("pekka", 45, "12345", "33333-44455", 5)
+
+    def test_kahden_saman_ostoksen_paaytyttya_pankin_metodilla_tilisiirto_oikeat_parametrit(self):
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.tilimaksu("pekka", "12345")
+        self.pankki_mock.tilisiirto.assert_called_with("pekka", 45, "12345", "33333-44455", 10)
+
+    def test_kahden_eri_ostoksen_paaytyttya_pankin_metodilla_tilisiirto_oikeat_parametrit(self):
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.lisaa_koriin(2)
+        self.kauppa.tilimaksu("pekka", "12345")
+        self.pankki_mock.tilisiirto.assert_called_with("pekka", 45, "12345", "33333-44455", 12)
+
+    def test_kahden_loppuvan_ostoksen_paaytyttya_pankin_metodilla_tilisiirto_oikeat_parametrit(self):
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.lisaa_koriin(3)
+        self.kauppa.tilimaksu("pekka", "12345")
+        self.pankki_mock.tilisiirto.assert_called_with("pekka", 45, "12345", "33333-44455", 5)
