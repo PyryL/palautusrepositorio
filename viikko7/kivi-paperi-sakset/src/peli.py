44843b95efb30b2570b8e5ed3b81376431f92d59
PyryL <pyry@pyry.info>
2022-12-19 09:34:14 +0200

    vähennetty toisteisuutta (v7t3)

--- viikko7/kivi-paperi-sakset/src/peli.py
+++ viikko7/kivi-paperi-sakset/src/peli.py
@@ -5,28 +5,27 @@ class Peli:
         tuomari = Tuomari()
         tekoaly = self._luo_tekoaly()
         
+        ekan_siirto, tokan_siirto = self._pelaa_kierros(tekoaly)
+
+        while self._onko_ok_siirto(ekan_siirto) and self._onko_ok_siirto(tokan_siirto):
+            tuomari.kirjaa_siirto(ekan_siirto, tokan_siirto)
+            print(tuomari)
+
+            ekan_siirto, tokan_siirto = self._pelaa_kierros(tekoaly)
+            if tekoaly is not None and callable(tekoaly.aseta_siirto):
+                tekoaly.aseta_siirto(ekan_siirto)
+
+        print("Kiitos!")
+        print(tuomari)
+
+    def _pelaa_kierros(self, tekoaly):
         ekan_siirto = input("Ensimmäisen pelaajan siirto: ")
         if tekoaly is None:
             tokan_siirto = input("Toisen pelaajan siirto: ")
         else:
             tokan_siirto = tekoaly.anna_siirto()
             print(f"Tietokone valitsi: {tokan_siirto}")
-
-        while self._onko_ok_siirto(ekan_siirto) and self._onko_ok_siirto(tokan_siirto):
-            tuomari.kirjaa_siirto(ekan_siirto, tokan_siirto)
-            print(tuomari)
-
-            ekan_siirto = input("Ensimmäisen pelaajan siirto: ")
-            if tekoaly is None:
-                tokan_siirto = input("Toisen pelaajan siirto: ")
-            else:
-                tokan_siirto = tekoaly.anna_siirto()
-                print(f"Tietokone valitsi: {tokan_siirto}")
-                if callable(tekoaly.aseta_siirto):
-                    tekoaly.aseta_siirto(ekan_siirto)
-
-        print("Kiitos!")
-        print(tuomari)
+        return (ekan_siirto, tokan_siirto)
 
     def _luo_tekoaly(self):
         return None
